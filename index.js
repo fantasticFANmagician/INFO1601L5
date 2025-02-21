6a2f8a38884744f7ca645d28f9ad6f48ee4b1fa0
fantasticmagici <39024306-fantasticmagici@users.noreply.replit.com>
2025-02-21 17:36:53 +0000

    User checkpoint: Add student data structures and functions to calculate average grade.

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+//object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+
+const students = [bob, sally, paul];
+
+function getAverageGrade(student, course){
+  for(let record of student.transcript){
+    if (record.course === course){
+      let sum = 0;
+      for(let grade of record.grades){
+        sum += grade;
+      }
+      return sum / record.grades.length;
+    }
+  }
+  return -1;
 }
+
+
+function avgBMI(people) {
+  let sum = 0;
+  for (let person of people) {
+    //sum the bmi of each person
+    sum += calcBMI(person.weight, person.height);
+  }
+
+  console.log (getAverageGrade(bob,INFO 1603))
